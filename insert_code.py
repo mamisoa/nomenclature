9d20d0fa2b20b36886ece4ccfdbf8a8963c626d9
mamisoa <mamisoa@gmail.com>
2024-05-20 14:56:55 +0200

    working insert/update

--- insert_code.py
+++ insert_code.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 
 from dotenv import dotenv_values
 from sqlalchemy import MetaData, create_engine, select
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
 
 config = dotenv_values(".env")
@@ -17,43 +17,51 @@ metadata = MetaData()
 
 # Reflect existing database tables into the new MetaData object
 metadata.reflect(bind=engine)
+Session = sessionmaker(bind=engine)
+
 
 def process_xml_and_update_db(xml_path):
-    # Parse the XML file
     tree = ET.parse(xml_path)
     root = tree.getroot()
-    
-    # Access the 'nomenclature' table
     nomenclature = metadata.tables['nomenclature']
 
-    with engine.connect() as connection:
+    session = Session()
+
+    try:
         for entry in root.findall('.//nomen_code_entry'):
             code = entry.get('code')
             desc_fr = entry.find('desc_fr').text if entry.find('desc_fr') is not None else ''
             desc_nl = entry.find('desc_nl').text if entry.find('desc_nl') is not None else ''
             code_desc = f"{desc_fr} {desc_nl}"
             cout = entry.find('cout').text if entry.find('cout') is not None else '[0,0,0,0]'
-            
-            # Prepare data
+
+            # Prepare data dictionary here
             data = {
                 "date": datetime.datetime.now(),
                 "code": code,
                 "code_desc": code_desc,
-                "price_list": cout
+                "price_list": cout,
+                "supplement_ratio": 1.52
             }
-            
-            # Check if the code already exists using the correct select syntax
-            stmt = select(func.count()).select_from(nomenclature).where(nomenclature.c.code == code)
-            result = connection.execute(stmt)
-            
-            if result.scalar() > 0:
+
+            # Check if the code already exists
+            result = session.execute(select(func.count()).select_from(nomenclature).where(nomenclature.c.code == code)).scalar()
+
+            if result > 0:
                 # Update existing record
                 update_stmt = nomenclature.update().where(nomenclature.c.code == code).values(**data)
-                connection.execute(update_stmt)
+                session.execute(update_stmt)
             else:
                 # Insert new record
                 insert_stmt = nomenclature.insert().values(**data)
-                connection.execute(insert_stmt)
+                session.execute(insert_stmt)
+
+        session.commit()  # Commit all changes
+    except Exception as e:
+        session.rollback()  # Rollback changes on error
+        print(f"Error: {e}")
+    finally:
+        session.close()  # Close the session
 
 xml_file_path = './transformed.xml'
 process_xml_and_update_db(xml_file_path)
